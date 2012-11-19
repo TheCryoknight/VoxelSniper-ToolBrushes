40ae79664a37a9e45bda1b71b8acc10251b15e24
Monofraps <nic_ristock@live.de>
2012-11-19 11:04:43 +0100

    * Turned package into bukkit plugin * Added main plugin class * Closes #1

---
+++ src/main/java/com/thevoxelbox/voxelsniper/tool/Plugin.java
@@ -0,0 +1,19 @@
+package com.thevoxelbox.voxelsniper.tool;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * 
+ * @author Monofraps
+ *
+ */
+public class Plugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
